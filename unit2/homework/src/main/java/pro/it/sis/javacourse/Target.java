c30f79e694de7c75231c310916a92a154163c8ec
Darkside1608 <darkside021@bk.ru>
2020-03-31 00:03:08 +0700

    Переделал

--- unit2/homework/src/main/java/pro/it/sis/javacourse/Target.java
+++ unit2/homework/src/main/java/pro/it/sis/javacourse/Target.java
@@ -2,6 +2,27 @@ package pro.it.sis.javacourse;
 
 public class Target {
 
+    private int physicalDamage;
+
+    private int fireDamage;
+
+    private int iceDamage;
+
+
+
+    public void takePhysicalDamage(int physicalDamage) {
+        this.physicalDamage += physicalDamage;
+    }
+
+
+    public void takeFireDamage(int fireDamage) {
+        this.fireDamage += fireDamage;
+    }
+
+    public void takeIceDamage(int iceDamage) {
+        this.iceDamage += iceDamage;
+    }
+
     public int getPhysicalDamage() {
         return physicalDamage;
     }
@@ -13,10 +34,4 @@ public class Target {
     public int getIceDamage() {
         return iceDamage;
     }
-
-    private int physicalDamage;
-
-    private int fireDamage;
-
-    private int iceDamage;
 }
