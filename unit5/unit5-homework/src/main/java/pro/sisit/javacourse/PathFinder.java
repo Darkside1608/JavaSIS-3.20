24b8e691f4fcfc3da7d1035db251d892d5a99971
Darkside1608 <darkside021@bk.ru>
2020-04-08 03:58:10 +0700

    hw5

--- unit5/unit5-homework/src/main/java/pro/sisit/javacourse/PathFinder.java
+++ unit5/unit5-homework/src/main/java/pro/sisit/javacourse/PathFinder.java
@@ -1,9 +1,12 @@
 package pro.sisit.javacourse;
 
 import pro.sisit.javacourse.optimal.DeliveryTask;
+import pro.sisit.javacourse.optimal.Route;
 import pro.sisit.javacourse.optimal.Transport;
 
-import java.util.List;
+import java.math.BigDecimal;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class PathFinder {
 
@@ -14,6 +17,45 @@ public class PathFinder {
      */
     public Transport getOptimalTransport(DeliveryTask deliveryTask, List<Transport> transports) {
         // ToDo: realize me!
-        return null;
+
+        Comparator<Transport> transportComparator =  Comparator.comparing(transport -> getTotalPriceByRoute(deliveryTask, transport));
+        return Optional.ofNullable(deliveryTask)
+                .flatMap(data -> Optional.ofNullable(transports))
+                .flatMap(data -> getAvailableTransport(deliveryTask, data)
+                        .stream()
+                        .min(transportComparator))
+                .orElse(null);
     }
+
+    private List<Transport> getAvailableTransport(DeliveryTask deliveryTask, List<Transport> transports){
+        return
+                transports.stream()
+                        .filter(transport -> filterByTransportType(deliveryTask, transport))
+                        .filter(transport -> filterByVolume(deliveryTask, transport))
+                        .collect(Collectors.toList());
+    }
+
+    private boolean filterByTransportType(DeliveryTask deliveryTask, Transport transport){
+        return
+                deliveryTask.getRoutes()
+                        .stream()
+                        .map(Route::getType)
+                        .collect(Collectors.toList())
+                        .contains(transport.getType());
+    }
+
+    private boolean filterByVolume(DeliveryTask deliveryTask, Transport transport){
+        return transport.getVolume().compareTo(deliveryTask.getVolume()) >= 0;
+    }
+
+    private BigDecimal getTotalPriceByRoute(DeliveryTask deliveryTask, Transport transport){
+        return
+                deliveryTask.getRoutes()
+                        .stream()
+                        .filter(route -> route.getType().equals(transport.getType()))
+                        .findAny()
+                        .map(route -> route.getLength().multiply(transport.getPrice()))
+                        .orElse(null);
+    }
+
 }
