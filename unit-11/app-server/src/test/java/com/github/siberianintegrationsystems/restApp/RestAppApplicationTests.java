75eff632eca29f6e1ee8d42091179bf6b02853a1
Darkside1608 <60291167+Darkside1608@users.noreply.github.com>
2020-04-27 05:10:28 +0700

    Update RestAppApplicationTests.java

--- unit-11/app-server/src/test/java/com/github/siberianintegrationsystems/restApp/RestAppApplicationTests.java
+++ unit-11/app-server/src/test/java/com/github/siberianintegrationsystems/restApp/RestAppApplicationTests.java
@@ -16,44 +16,45 @@ import static org.junit.Assert.assertNotNull;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class RestAppApplicationTests{
-	private final JournalService journalService;
-	private final QuestionService questionService;
-	private final SessionService sessionService;
-	private final SessionRestController sessionRestController;
-	private final QuestionRestController questionRestController;
-	private final JournalRestController journalRestController;
-	private final SessionRepository sessionRepository;
-	private final QuestionRepository questionRepository;
-	private final JournalRepository journalRepository;
-	private final AnswerRepository answerRepository;
-	private final SelectedAnswerRepository selectedAnswerRepository;
+public class RestAppApplicationTests {
 
-	public RestAppApplicationTests(JournalService journalService,
-								   QuestionService questionService,
-								   SessionService sessionService,
-								   SessionRestController sessionRestController,
-								   QuestionRestController questionRestController,
-								   JournalRestController journalRestController,
-								   SessionRepository sessionRepository,
-								   QuestionRepository questionRepository,
-								   JournalRepository journalRepository,
-								   AnswerRepository answerRepository,
-								   SelectedAnswerRepository selectedAnswerRepository) {
-		this.journalService = journalService;
-		this.questionService = questionService;
-		this.sessionService = sessionService;
-		this.sessionRestController = sessionRestController;
-		this.questionRestController = questionRestController;
-		this.journalRestController = journalRestController;
-		this.sessionRepository = sessionRepository;
-		this.questionRepository = questionRepository;
-		this.journalRepository = journalRepository;
-		this.answerRepository = answerRepository;
-		this.selectedAnswerRepository = selectedAnswerRepository;
-	}
-
+	@Autowired
+	private JournalService journalService;
+	@Autowired
+	private QuestionService questionService;
+	@Autowired
+	private SessionService sessionService;
+	@Autowired
+	private JournalRestController journalRestController;
+	@Autowired
+	private QuestionRestController questionRestController;
+	@Autowired
+	private SessionRestController sessionRestController;
+	@Autowired
+	private AnswerRepository answerRepository;
+	@Autowired
+	private JournalRepository journalRepository;
+	@Autowired
+	private QuestionRepository questionRepository;
+	@Autowired
+	private SelectedAnswerRepository selectedAnswerRepository;
+	@Autowired
+	private SessionRepository sessionRepository;
 	@Test
+	public void contextLoads() throws Exception{
+		assertNotNull(journalService);
+		assertNotNull(questionService);
+		assertNotNull(sessionService);
+		assertNotNull(journalRestController);
+		assertNotNull(questionRestController);
+		assertNotNull(sessionRestController);
+		assertNotNull(answerRepository);
+		assertNotNull(journalRepository);
+		assertNotNull(questionRepository);
+		assertNotNull(selectedAnswerRepository);
+		assertNotNull(sessionRepository);
+	}
+}
 	public void contextLoads() {
 		assertNotNull(journalService);
 		assertNotNull(questionService);
